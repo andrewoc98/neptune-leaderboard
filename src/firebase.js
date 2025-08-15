0b667a139680b35d024c2b8d704e11ac93f8e37c
Andrew O Connor <173258169+andrewoconnorBetFanatics@users.noreply.github.com>
2025-08-15 12:17:00 +0100

    Admin stuff

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,5 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import {collection, getDocs, getFirestore} from "firebase/firestore";
 import { doc, setDoc, deleteDoc } from "firebase/firestore";
 import {v4 as uuidv4} from 'uuid';

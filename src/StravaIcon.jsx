e407f72dbc058aa8529b9cacd5718b7213b2c554
Andrew O Connor <173258169+andrewoconnorBetFanatics@users.noreply.github.com>
2025-09-01 16:13:30 +0100

    Strava

--- src/StravaIcon.jsx
+++ src/StravaIcon.jsx
@@ -1,7 +1,7 @@
 export function StravaIcon() {
     return (
             <img
-                src="./src/brand-strava-svgrepo-com.svg"
+                src="./brand-strava-svgrepo-com.svg"
                 alt="Strava Logo"
                 width={40}   // adjust size
                 height={40}  // adjust size
